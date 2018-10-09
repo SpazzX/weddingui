9dff712d94e02696b7d7e35f6aa060324c178ee8
SpazzX <michael88savier@gmail.com>
2018-10-09 18:47:20 -0400

    Updating install.  Need to update the GuestListContainer to handle no guests.  Cheating on the SPA concept with window.location.reload(); to see data come back in real time after hitting mongo backend.

--- src/components/common/GuestList.js
+++ src/components/common/GuestList.js
@@ -1,60 +1,42 @@
 import React from 'react';
 import axios from 'axios';
+import { Form } from 'antd';
+
+const FormItem = Form.Item;
 
 export default class GuestList extends React.Component {
   state = {
-    firstName: '',
-    lastName: ''
-  }
+    fName: '',
+    lName: ''
+  };
 
-  handleChangeFirstName = event => {
-    this.setState({ firstName: event.target.value });
-  }
-
-  handleChangeLastName = event => {
-    this.setState({ lastName: event.target.value });
-  }
+  handlefNameChange = event => {this.setState({ fName: event.target.value })};
+  handlelNameChange = event => {this.setState({ lName: event.target.value })};
 
   handleSubmit = event => {
     event.preventDefault();
 
-    const firstName = {
-      firstName: this.state.firstName
-    };
-    const lastName = {
-      lastName: this.state.lastName
-    };
-
-    axios.post(`http://localhost:12345/api/guest`, { firstName: firstName, lastName: lastName })
+    axios.post('http://localhost:12345/api/guest',
+      { firstName: this.state.fName, lastName: this.state.lName})
       .then(res => {
         console.log(res);
         console.log(res.data);
+        //window.location.reload();
       })
   }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <table>
-          <tbody>
-            <tr>
-            <label>
-              Person FirstName:
-              <input type="text" name="firstName" onChange={this.handleChangeFirstName} />
-            </label>
-            </tr>
-            <tr>
-            <label>
-              Person LastName:
-              <input type="text" name="lastName" onChange={this.handleChangeLastName} />
-            </label>
-            </tr>
-          </tbody>
-          </table>
-          <button type="submit">Add</button>
-        </form>
-      </div>
+
+      <Form onSubmit={this.handleSubmit}>
+        <FormItem>
+          <label>First Name:  <input type="text" name="this.state.fName" onChange={this.handlefNameChange} /></label>
+        </FormItem>
+        <FormItem>
+          <label>Last Name:  <input type="text" name="this.state.lName" onChange={this.handlelNameChange} /></label><br />
+        </FormItem>
+        <button type="submit">Add</button>
+      </Form>
     )
   }
 }
